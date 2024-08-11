755fe93d49ff4fe97d903ad27d91ddd345346448
Felipe Borges <felipetecacc@gmail.com>
2024-08-11 17:56:09 -0300

    [ HTML ] Projects Component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IProjects } from '../../interface/IProjects.interface';
 
 @Component({
   selector: 'app-projects',
@@ -9,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  public arrayProjects = signal<IProjects[]>([
+    {
+      src: 'assets/img/projects/lista-de-tarefas.png',
+      alt: 'Projeto Lista de Tarefas',
+      title: 'Lista de Tarefas',
+      width: '100px',
+      height: '51px',
+      description: '<p>descrição do projeto</p>',
+      links: [
+        {
+          name: 'Link do projeto',
+          href: 'https://exemplo.com.br'
+        }
+      ],
+    },
+  ]);
 }
