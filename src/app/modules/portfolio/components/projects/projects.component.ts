ec26852aeb47dec9ac331e7a228ceb7babf05c1e
Felipe Borges <felipetecacc@gmail.com>
2024-08-30 21:46:47 -0300

    [ DIALOG ] Projects Component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,5 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
+// Interface
 import { IProjects } from '../../interface/IProjects.interface';
+
+// Material
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+// Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
@@ -9,6 +20,7 @@ import { IProjects } from '../../interface/IProjects.interface';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
 
   public arrayProjects = signal<IProjects[]>([
     {
@@ -26,4 +38,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    });
+  }
 }
